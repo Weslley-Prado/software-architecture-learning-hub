ee11f62b38dc8a35791413bff422d671b899acb9
Weslley-Prado <weslley.prado73@gmail.com>
2024-09-17 18:36:38 -0300

    main: create client

---
+++ Intermediate/restapiapplication/src/main/java/com/weslleyprado/restapiapplication/RestApiApplicationClientApplication.java
@@ -0,0 +1,13 @@
+package com.weslleyprado.restapiapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiApplicationClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiApplicationClientApplication.class, args);
+	}
+
+}
---
+++ Intermediate/restapiapplication/src/test/java/com/weslleyprado/restapiapplication/RestApiApplicationClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weslleyprado.restapiapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
